96fcec7c0502a50e4b6e788cc802d27184039018
lckytrzqjbd <luckytakyta142@gmail.com>
2025-09-04 18:45:18 +0700

    Update and rename README.md to penjumlahan bilangan bulat kecil.cpp

---
+++ penjumlahan bilangan bulat kecil.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+int A ;
+int B;
+cout<< "program ini hanya dapat menjumlahkan bilangan bulat kecil saja yaa\n" ;
+cout << "Masukkan nilai pertama: ";
+cin >> A ;
+cout << "Masukkan nilai kedua: ";
+cin >> B;
+cout << "jadi, " << A << "+" <<B;
+cout << "=" << A+B << endl;
+return 0;
+}
